99f9ee6944b59467a10f8875ddf9e33e5168c678
Arunkumar Palani <arunkumarpas002@gmail.com>
2018-09-05 12:02:47 +0530

    Add files via upload

---
+++ constructor.java
@@ -0,0 +1,14 @@
+class cons
+	{
+		cons()
+			{
+				System.out.println("This is the example of constructor");
+			}
+	}
+class constructor
+	{
+		public static void main(String a[])
+			{
+				cons obj=new cons();
+			}
+	}
